baa2b934da2b58f1c26c5eb6af45804dbe98937f
Evgeny Savelev <b.boy.bip@gmail.com>
2019-10-03 00:02:30 +0200

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,79 @@
+"use strict";
+
+var siteCSS = '/frontend/Pearl/weltpixel_custom/en_AU/css/style.css" />',
+    siteUrl = 'http://dev.mensbiz.com.au/';
+
+
+var gulp = require('gulp'),
+    rename = require('gulp-rename'),
+    notify = require('gulp-notify'),
+    prefix = require('gulp-autoprefixer'),
+    connect = require('gulp-connect'),
+    sass = require('gulp-sass'),
+    plumber = require('gulp-plumber'),
+    sourcemaps = require('gulp-sourcemaps'),
+		browserSync = require('browser-sync'),
+		cssnano = require('gulp-cssnano');
+
+
+//css inject
+gulp.task('css-inject', function () {
+    var config = {
+      addSourceMaps: true,
+      concatCSS: true,
+      plugins:{
+        cleanCss: {}
+      }
+    };
+    var reload = browserSync.reload;
+    return gulp.src('src/scss/common.scss')
+    .pipe(plumber({ // plumber - плагин для отловли ошибок.
+            errorHandler: notify.onError(function(err) { // nofity - представление ошибок в удобном для вас виде.
+                return {
+                    title: 'Styles',
+                    message: err.message
+                }
+            })
+        }))
+    // .pipe(sourcemaps.init())
+    .pipe(sass()) //Компиляция sass.
+    .pipe(prefix('last 2 versions','> 1%', 'ie 9'))
+		.pipe(rename('style.css'))
+		// .pipe(sourcemaps.write())
+		.pipe(cssnano())
+    .pipe(gulp.dest('app/assets//css'))
+    .pipe(reload({stream:true}));
+});
+
+
+//watch
+gulp.task('watch', function () {
+    gulp.watch('src/scss/**/*.*', gulp.series('css'));
+    gulp.watch('app/*.html', gulp.series('html'));
+});
+
+//watch-inject
+gulp.task('watch-inject', function () {
+    gulp.watch('src/scss/**/*.*', gulp.series('css-inject'));
+});
+
+
+//server
+gulp.task('server', function() {
+    browserSync({
+        proxy: siteUrl,
+        middleware: require('serve-static')('./app'),
+        rewriteRules: [ 
+        {
+            match: new RegExp (siteCSS),
+            fn: function() {
+                return '/frontend/Pearl/weltpixel_custom/en_AU/css/style_del.css" /><link rel="stylesheet" href="http://dev.mensbiz.com.au/assets/css/style.css">';
+            }
+        }
+        ]
+    });
+});
+
+
+
+gulp.task('inject', gulp.parallel('css-inject', 'watch-inject', 'server'));
